4355dae54c5cbc88bfe1efc947809bf9f1976473
DanielRueda01 <rueda.chavez.daniel@gmail.com>
2023-04-17 16:41:43 -0600

    Cambios 3

--- HungrySquidJuego/Assets/Scripts/Juego/movRandomPez.cs
+++ HungrySquidJuego/Assets/Scripts/Juego/movRandomPez.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class movRandomPez : MonoBehaviour
 {
+    float nextXPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //float x = Random.Range(-1f, 1f);
+        //Rigidbody2D.AddForce(new Vector2(x, 0f * 100f) * 100f, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
+
+
     }
 }
